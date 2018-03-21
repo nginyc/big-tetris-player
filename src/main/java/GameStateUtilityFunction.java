fe24f2058eeeecbd6a5ae468d511e0f4d07cbef0
Ngin Yun Chuan <yunchuann@gmail.com>
2018-03-21 23:21:52 +0800

    Fix bug due to mishandling of int VS double

--- src/main/java/GameStateUtilityFunction.java
+++ src/main/java/GameStateUtilityFunction.java
@@ -10,8 +10,8 @@ public class GameStateUtilityFunction implements IGameStateUtilityFunction {
 		this.weights = weights;
 	}
 
-	private double getWeight(int i) {
-		return this.weights[i];
+	private double normalize(double value, double min, double max) {
+		return (value - min) / (max - min);
 	}
 
 	public double get(GameState gameState) {
@@ -25,21 +25,21 @@ public class GameStateUtilityFunction implements IGameStateUtilityFunction {
         // worst/best cases which are basically almost impossible to attain.
         
 		return (
-			this.getWeight(0) * (gameState.getColumnAggregateHeight() / col / row) +                    // 0 to col * row
-			this.getWeight(1) * (gameState.getRowsClearedInPrevMove() / 4) +                            // 0 to 4
-			this.getWeight(2) * (gameState.getHolesTotalVolume() / col / (row - 1)) +                   // 0 to col * (row - 1)
-			this.getWeight(3) * (gameState.getMaxTopHeight() / row) +                                   // 0 to row
-			this.getWeight(4) * (gameState.getBumpiness() / row / row / (col - 1)) +                    // 0 to row * row * (col - 1)
-			this.getWeight(5) * (gameState.getWells() / row / row / (col - 1) * 2) +                    // 0 to row * row * ((col - 1) / 2)
-			this.getWeight(6) * (gameState.getPrevLandingHeight() / row) +                              // 0 to row
-			this.getWeight(7) * (gameState.getColTransitions() / col / row) +                           // 0 to row * col
-			this.getWeight(8) * (gameState.getRowTransitions() / col / row) +                           // 0 to row * col
-			this.getWeight(9) * (gameState.getBlockadesTotalVolume() / col / (row - 1)) +               // 0 to (row - 1) * col
-			this.getWeight(10) * (gameState.getNumBlocksInField() / row / (col - 1)) +                  // 0 to row * (col - 1)
-			this.getWeight(11) * (gameState.getMeanHeightDifference() / row / (col / 2) / (col / 2)) +  // 0 to row * (col / 2)^2
-            this.getWeight(12) * (gameState.getNumEdgesTouchingTheWall() / row / 2) +                   // 0 to row * 2
-            this.getWeight(13) * (gameState.getNumEdgesTouchingTheFloor() / col) +                      // 0 to col
-            this.getWeight(14) * (gameState.getNumEdgesTouchingCeiling() / col)                         // 0 to col
+			this.weights[0] * (this.normalize(gameState.getColumnAggregateHeight(), 0, col * row)) +                    // 0 to col * row
+			this.weights[1] * (this.normalize(gameState.getRowsClearedInPrevMove(), 0, 4)) +                            // 0 to 4
+			this.weights[2] * (this.normalize(gameState.getHolesTotalVolume(), 0, col * (row - 1))) +                   // 0 to col * (row - 1)
+			this.weights[3] * (this.normalize(gameState.getMaxTopHeight(), 0, row)) +                                   // 0 to row
+			this.weights[4] * (this.normalize(gameState.getBumpiness(), 0, row * row * (col - 1))) +                    // 0 to row * row * (col - 1)
+			this.weights[5] * (this.normalize(gameState.getWells(), 0, row * row * (col - 1) * 2)) +                    // 0 to row * row * ((col - 1) / 2)
+			this.weights[6] * (this.normalize(gameState.getPrevLandingHeight(), 0, row)) +                              // 0 to row
+			this.weights[7] * (this.normalize(gameState.getColTransitions(), 0, col * row)) +                           // 0 to row * col
+			this.weights[8] * (this.normalize(gameState.getRowTransitions(), 0, col * row)) +                           // 0 to row * col
+			this.weights[9] * (this.normalize(gameState.getBlockadesTotalVolume(), 0, col * (row - 1))) +               // 0 to (row - 1) * col
+			this.weights[10] * (this.normalize(gameState.getNumBlocksInField(), 0, row * (col - 1))) +                  // 0 to row * (col - 1)
+			this.weights[11] * (this.normalize(gameState.getMeanHeightDifference(), 0, (double)(row * col * col) / 4)) +  // 0 to row * (col / 2)^2
+            this.weights[12] * (this.normalize(gameState.getNumEdgesTouchingTheWall(), 0, row * 2)) +                   // 0 to row * 2
+            this.weights[13] * (this.normalize(gameState.getNumEdgesTouchingTheFloor(), 0, col)) +                      // 0 to col
+            this.weights[14] * (this.normalize(gameState.getNumEdgesTouchingCeiling(), 0, col))                         // 0 to col
 		);
 	}
 }
