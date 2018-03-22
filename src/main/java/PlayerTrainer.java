6655bf71f21f46eb8834ed94d3c9cf86019895ae
Ngin Yun Chuan <yunchuann@gmail.com>
2018-03-22 23:06:51 +0800

    Add more results after training with normalized weights

--- src/main/java/PlayerTrainer.java
+++ src/main/java/PlayerTrainer.java
@@ -6,14 +6,34 @@ public class PlayerTrainer {
 	}
 
 	public static void main(String[] args) {
-		// TODO: GA stops on converge
-		// TODO: Hill climbing on best performing indiv
+		while (true) {
+			trainGaThenSa(16);	
+		}	
+	}
+
+	private static void trainGa(int rows) {
 		GameStateUtilityLearner learner = new GameStateUtilityLearner(
-			20, 15, 10, 100, 100, 0.1, 0.8, 0.5
+			rows, 15, 10, 100, 100, 0.1, 0.8, 0.5
 		);
 		double[] weights = learner.train();
-		System.out.println("Weights: " + Arrays.toString(weights));
+		System.out.println("Weights found: " + Arrays.toString(weights));
+		
+		Evaluator evaluator = new Evaluator();
+		double rowsCleared = evaluator.evaluate(weights);
+		System.out.println("Rows cleared: " + rowsCleared);
+	}
 
+	private static void trainGaThenSa(int rows) {
+		GameStateUtilityLearner learner = new GameStateUtilityLearner(
+			rows, 15, 10, 100, 100, 0.1, 0.8, 0.5
+		);
+		double[] weights = learner.train();
+		System.out.println("Weights found in GA: " + Arrays.toString(weights));
+
+		SimulatedAnnealingLearner saLearner = new SimulatedAnnealingLearner(rows, 15, 10, 100000, 0.003);
+		weights = saLearner.train(weights);
+		System.out.println("Weights found after SA: " + Arrays.toString(weights));
+		
 		Evaluator evaluator = new Evaluator();
 		double rowsCleared = evaluator.evaluate(weights);
 		System.out.println("Rows cleared: " + rowsCleared);
